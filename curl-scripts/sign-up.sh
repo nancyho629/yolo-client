d8a7093fda549bbfab12a7f6dd181ec131c2decf
nancyho629 <nancyho629@gmail.com>
2019-09-26 09:30:28 -0400

    Add create and sign-up curl scripts
    Added curl script for create item
    Added curl script for sign up

---
+++ curl-scripts/sign-up.sh
@@ -0,0 +1,13 @@
+curl "http://localhost:4741" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials":{
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
