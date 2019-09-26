0c6058d4a3677b4fad335e0e5ac1b096a58b9874
mrodrz7 <m.rodriguez0743@gmail.com>
2019-09-26 09:30:41 -0400

    Add PATCH curl-script
    - Add PATCH curl-script (change password) for auth

---
+++ curl-scripts/change-password.sh
@@ -0,0 +1,13 @@
+curl "https://localhost:4741" \
+  --include \
+  --request PATCH \
+  --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "passwords": {
+      "old": "'"${OLDPW}"'",
+      "new": "'"${NEWPW}"'"
+    }
+  }'
+
+echo
