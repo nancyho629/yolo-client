c31e2312e12e9e8db3467f78efb11243b6c95133
elinag <elina.gorshkova@icloud.com>
2019-09-26 11:22:14 -0400

    Add event listener to app.js
    Loads auth forms handlebar on document.ready

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,8 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const signInForm = require('./templates/auth.handlebars')
 
 $(() => {
-  // your JS code goes here
+  $('.main').html(signInForm)
 })
