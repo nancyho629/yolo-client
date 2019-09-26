0c5714855b44a8e1731209c263df3d7e823b37d6
nancyho629 <nancyho629@gmail.com>
2019-09-26 10:37:00 -0400

    Re-add sign up functionality after rebasing corrections

--- assets/scripts/auth/event.js
+++ assets/scripts/auth/event.js
@@ -20,7 +20,15 @@ const onSignIn = function (event) {
     .catch(ui.failure)
 }
 
+const onSignUp = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
 module.exports = {
   onSignOut,
-  onSignIn
+  onSignIn,
+  onSignUp
 }
